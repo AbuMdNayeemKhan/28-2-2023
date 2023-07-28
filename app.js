a906b2ec9514782bd5dadf094760923a2042dbd2
unknown <abumohammadnayemkhan@gmail.com>
2023-07-29 00:07:53 +0600

    learning JavaScript

---
+++ app.js
@@ -0,0 +1,32 @@
+let myArray = [30, 20, 40, 10, 50, 80, 90, 70, 60];
+
+//====================ARRAY REVERSE START====================//
+//let reverseArray = myArray.reverse();
+//console.log(reverseArray);
+//====================ARRAY REVERSE ENDED====================//
+
+//====================ARRAY PUSH START====================//
+//myArray.push(100);
+//====================ARRAY PUSH ENDED====================//
+
+//====================ARRAY POP START====================//
+// myArray.pop();
+//====================ARRAY POP ENDED====================//
+
+
+
+//====================ARRAY SORT START====================//
+//myArray.sort();
+//====================ARRAY SORT ENDED====================//
+
+//====================ARRAY SORT REVEARCH START====================//
+// myArray.sort().reverse();
+//====================ARRAY SORT REVEARCH ENDED====================//
+
+//====================ARRAY SLICE START====================//
+// let result = myArray.slice(1, 3);
+//====================ARRAY SLICE ENDED====================//
+
+//====================PRINTING DATA START====================//
+console.log(result);
+//====================PRINTING DATA ENDED====================//
